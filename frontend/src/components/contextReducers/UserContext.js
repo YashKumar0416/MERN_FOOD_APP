eb794aa055ccb1b4496efc9973ab54a219c1f243
YashKumar0416 <122213030+YashKumar0416@users.noreply.github.com>
2023-03-17 00:06:31 +0530

    Delete frontend directory

--- frontend/src/components/contextReducers/UserContext.js
+++
@@ -1,8 +0,0 @@
-export const initialUser = {isAuthenticated: false, name: '', email: '', phone: '', address: ''}
-
-export const userReducer = (state, action)=> {
-    if(action.type === "USER") {
-        return {...state, isAuthenticated: action.isAuthenticated , name: action.name, email:action.email, phone: action.phone, address: action.address}
-    }    
-    return state;
-}
